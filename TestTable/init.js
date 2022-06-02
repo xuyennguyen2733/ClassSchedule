b6792370e457b201574e6e41e4b5c7b499c023a9
THI TRIEU XUYEN NGUYEN <u1252970@umail.utah.edu>
2022-06-01 20:23:52 -0600

    Fixed shadows and fix course form logic in test table

--- TestTable/init.js
+++ TestTable/init.js
@@ -14,13 +14,31 @@
 /****************************************************** 
  GLOBAL VERIABLES
  ******************************************************/
+const weekdayList = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+const hourList = [];
+for (let i = 0; i < 10; i++) {
+  const offset = 7;
+  const time = indexToCalendarTime(i, offset);
+  hourList.push(time);
+}
+
 const timetableHead = document.querySelector(".timetable-head");
 const timetableBody = document.querySelector(".timetable-body");
 const timetableSide = document.querySelector(".timetable-side");
 
+const courseForm = document.querySelector(".course-form-container");
+const btnCloseCourseForm = courseForm.querySelector(".btn--close");
+const btnSaveCourseForm = courseForm.querySelector(".save-course");
+
 const startTime = {
-  startHour: 0,
-  startMinute: 0,
+  hour: 0,
+  minute: 0,
+  meridiem: "",
+};
+
+const endTime = {
+  hour: 0,
+  minute: 0,
   meridiem: "",
 };
 
@@ -29,7 +47,6 @@ const startTimeSelectEls = {
   minute: document.querySelector(".start-time #minute"),
   meridiem: document.querySelector(".start-time #meridiem"),
 };
-
 const endTimeSelectEls = {
   hour: document.querySelector(".end-time #hour"),
   minute: document.querySelector(".end-time #minute"),
@@ -55,11 +72,10 @@ INITIAL SETUP
   timetableHead.appendChild(cornerCell);
 
   // 2. next cells are the labels of weekdays
-  const classList = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
   for (let i = 0; i < 5; i++) {
     others = {
       classList: ["cell-color--2", "flex-center"],
-      textContent: classList[i],
+      textContent: weekdayList[i],
     };
     const weekdayBlock = createTimetableCell("weekday-label", others);
     timetableHead.appendChild(weekdayBlock);
@@ -67,29 +83,40 @@ INITIAL SETUP
 }
 
 // create timetable body cells
-for (let i = 1; i <= 40; i++) {
-  const others = {
-    gridRow: `${i} / ${i + 1}`,
-    classList: [`cell-color--${(i + 1) % 2}`],
-  };
-  const monBlock = createTimetableCell("monday", others);
-  const tueBlock = createTimetableCell("tuesday", others);
-  const wedBlock = createTimetableCell("wednesday", others);
-  const thuBlock = createTimetableCell("thursday", others);
-  const friBlock = createTimetableCell("friday", others);
-  //   console.log(monBlock);
-  timetableBody.appendChild(monBlock);
-  timetableBody.appendChild(tueBlock);
-  timetableBody.appendChild(wedBlock);
-  timetableBody.appendChild(thuBlock);
-  timetableBody.appendChild(friBlock);
+{
+  for (let i = 1; i <= 40; i++) {
+    const others = {
+      gridRow: `${i} / ${i + 1}`,
+      classList: [`cell-color--${(i + 1) % 2}`, "hover-animation"],
+    };
+
+    for (let j = 0; j < 5; j++) {
+      // 1. create the cell
+      const cell = createTimetableCell(weekdayList[j % 5], others);
+
+      // 2. add event listener to each cell
+      cell.addEventListener("click", showCourseForm);
+
+      // 3. add cell to body
+      timetableBody.appendChild(cell);
+    }
+    //   const tueBlock = createTimetableCell("tuesday", others);
+    //   const wedBlock = createTimetableCell("wednesday", others);
+    //   const thuBlock = createTimetableCell("thursday", others);
+    // const friBlock = createTimetableCell("friday", others);
+
+    // timetableBody.appendChild(monBlock);
+    // timetableBody.appendChild(tueBlock);
+    // timetableBody.appendChild(wedBlock);
+    // timetableBody.appendChild(thuBlock);
+    // timetableBody.appendChild(friBlock);
+  }
 }
 
 // create timetable side cells - starting from 8AM
 for (let i = 1; i <= 10; i++) {
-  const offset = 7;
-  const hour = i + offset <= 12 ? i + offset : i + offset - 12;
-  const meridiem = i + offset < 12 ? "AM" : "PM";
+  const hour = hourList[i - 1][0];
+  const meridiem = hourList[i - 1][1];
   const textContent = hour + meridiem;
 
   const others = {
@@ -106,9 +133,9 @@ for (let i = 1; i <= 10; i++) {
 }
 
 // Create hour droplist
-for (let i = 1; i <= 12; i++) {
-  const startOptionEl = createOptionElement(i, i);
-  const endOptionEl = createOptionElement(i, i);
+for (let i = 0; i < 10; i++) {
+  const startOptionEl = createOptionElement(i, hourList[i][0]);
+  const endOptionEl = createOptionElement(i, hourList[i][0]);
   startTimeSelectEls.hour.appendChild(startOptionEl);
   endTimeSelectEls.hour.appendChild(endOptionEl);
 }
@@ -130,14 +157,54 @@ EVENT SETUP
 ******************************************************/
 
 // Event listeners
+btnCloseCourseForm.addEventListener("click", hideCourseForm);
+btnSaveCourseForm.addEventListener("click", logCourse);
+
+startTimeSelectEls.hour.addEventListener("change", updateCourseTime);
+startTimeSelectEls.minute.addEventListener("change", updateCourseTime);
+
+endTimeSelectEls.hour.addEventListener("change", updateCourseTime);
+endTimeSelectEls.minute.addEventListener("change", updateCourseTime);
 
 // startTimeSelectEls.hour.addEventListener("change", startHourChanged);
 
 // timetable.
 
 // Event handlers
-function startHourChanged() {
-  console.log(this);
+
+function showCourseForm() {
+  const el = this;
+  const row = el.style.gridRow;
+  const rowStart = row.split(" / ")[0];
+
+  const index = Math.trunc((rowStart - 1) / 4);
+
+  console.log(rowStart, hourList[index]);
+  courseForm.classList.remove("hidden");
+}
+
+function hideCourseForm() {
+  courseForm.classList.add("hidden");
+}
+
+function logCourse() {
+  const cmp = compareTime(startTime, endTime);
+
+  if (cmp >= 0) console.log("invalid input");
+  else console.log("saved");
+}
+
+function updateCourseTime() {
+  startTime.hour = hourList[startTimeSelectEls.hour.value][0];
+  startTime.meridiem = hourList[startTimeSelectEls.hour.value][1];
+  startTime.minute = startTimeSelectEls.minute.value;
+
+  endTime.hour = hourList[endTimeSelectEls.hour.value][0];
+  endTime.meridiem = hourList[endTimeSelectEls.hour.value][1];
+  endTime.minute = endTimeSelectEls.minute.value;
+
+  startTimeSelectEls.meridiem.textContent = startTime.meridiem;
+  endTimeSelectEls.meridiem.textContent = endTime.meridiem;
 }
 
 /******************************************************
@@ -193,6 +260,22 @@ function createTimetableCell(classAttribute, othersObj) {
   }
 
   return divTag;
+}
+
+function indexToCalendarTime(index, offset) {
+  const hour = index + offset + 1;
+  return [hour <= 12 ? hour : hour - 12, hour < 12 ? "AM" : "PM"];
+}
+
+function compareTime(startTime, endTime) {
+  if (startTime.meridiem === "AM" && endTime.meridiem == "PM") return -1;
+  // console.log(startTime.hour === endTime.hour && startTime.minute === endTime.minute && startTime.meridiem === endTime.meridiem);
+  if (startTime.hour < endTime.hour) return -1;
+  if (startTime.minute < endTime.minute) return -1;
+
+  if (startTime.hour === endTime.hour && startTime.minute === endTime.minute && startTime.meridiem === endTime.meridiem) return 0;
+
+  return 1;
 }
 
 // function createDivElementOnGrid(classAttribute, gridColumn) {}
